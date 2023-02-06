dfe91be531c5ae699aefa1a1dc16874e9c870784
Ivan Ho <41582274+Corppet@users.noreply.github.com>
2023-02-06 17:52:32 -0500

    Add project files.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public static GameManager instance { private set; get; }
+}
